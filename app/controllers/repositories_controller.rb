057edc99146bcedd7df2568e0403d0fd823d548c
Tina Hang <mstinahang@gmail.com>
2018-03-07 03:13:22 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -6,6 +6,6 @@ class RepositoriesController < ApplicationController
 
   def create
     resp = Faraday.post "https://api.github.com/user/repos", {name: params[:name]}.to_json, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
-    
+    redirect_to '/'
   end
 end
