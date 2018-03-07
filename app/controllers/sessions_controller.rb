a0056fd9e133a574c074b0ca8965daad2d6258ae
Tina Hang <mstinahang@gmail.com>
2018-03-07 03:13:58 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@ class SessionsController < ApplicationController
 
   def create
     resp = Faraday.post "https://github.com/login/oauth/access_token", {client_id: ENV['GITHUB_ID'], client_secret:  ENV['GITHUB_SECRET'], code: params[:code]}, {'Accept' => 'application/json'}
+    auth_hash = JSON.parse(resp.body)
     
   end
   
